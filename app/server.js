951fd8780387374309ed993c53fa59263e9d18a2
Swolebrain <vic574m@gmail.com>
2016-02-05 21:53:08 -0500

    finished registration template

---
+++ app/server.js
@@ -0,0 +1,11 @@
+var port = 3001;
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get("/", function(req, res){
+  res.sendFile(path.resolve("../views/register.html"));
+});
+
+
+app.listen(port);
