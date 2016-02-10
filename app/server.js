184559a8e7a87c2de80100cf6f7a5c830639b84f
Swolebrain <vic574m@gmail.com>
2016-02-09 20:53:27 -0500

    form working

--- app/server.js
+++ app/server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use( express.static('../views'));
+//more info on serving static files:
+// http://expressjs.com/en/starter/static-files.html
+
 app.get("/", function(req, res){
   res.sendFile(path.resolve("../views/register.html"));
 });
