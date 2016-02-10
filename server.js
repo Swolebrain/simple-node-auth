a69ba00b1edfaa9ef3186263d9a613f62e80a435
Swolebrain <vic574m@gmail.com>
2016-02-10 18:39:21 -0500

    start of day 2

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use( express.static('../views'));
+
+//app.use( express.static('views') );
 //more info on serving static files:
 // http://expressjs.com/en/starter/static-files.html
 
 app.get("/", function(req, res){
-  res.sendFile(path.resolve("../views/register.html"));
+  res.sendFile(path.resolve("views/register.html"));
 });
+
+//example of handling request parameters:
+
 
 
 app.listen(port);
